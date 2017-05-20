74cf7bc8a2283d5d1acebc3b799bf90d5be9ad07
Виктория Владимирова <viktoria@iMac-Viktoria.local>
2017-05-20 13:08:57 +0500

    Тестовое задание

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path');
+
+const buildDir = path.join(__dirname, 'public');
+
+module.exports = {
+    entry: './app/main.js',
+
+    output: {
+        path: buildDir,
+        filename: 'bundle.js'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader?presets[]=es2015',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.hbs/,
+                loader: 'handlebars-template-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader'})
+            }
+        ]
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(buildDir),
+        new HtmlWebpackPlugin({
+            filename: 'page.html',
+            template: './app/templates/page.hbs',
+            data: require('./app/data/data.json')
+        }),
+        new ExtractTextPlugin('styles.css')
+    ]
+};
